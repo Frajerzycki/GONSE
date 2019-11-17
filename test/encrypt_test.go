e6bf8e9e57b3def5f2f14c91368346d300c84efe
ikcilrep <frajerzycki@protonmail.com>
2019-11-18 00:08:28 +0100

    Begin writing unit tests

---
+++ test/encrypt_test.go
@@ -0,0 +1,44 @@
+package test
+
+import (
+	"bytes"
+	"github.com/ikcilrep/gonse/pkg/nse"
+	"math/big"
+	"math/rand"
+	"testing"
+)
+
+func TestEncrypt(t *testing.T) {
+	for i := 1; i <= 512; i++ {
+		data := make([]byte, i)
+		keyBytes := make([]byte, 32)
+		salt := make([]byte, 16)
+		IV, err := nse.GenerateIV(i)
+		if err != nil {
+			t.Error(err)
+		}
+		if len(IV) != i {
+			t.Errorf("IV length %v is not data length %v", len(IV), i)
+		}
+		key := new(big.Int)
+		rand.Read(data)
+		rand.Read(keyBytes)
+		rand.Read(salt)
+		key.SetBytes(keyBytes)
+		ciphertext, err := nse.Encrypt(data, salt, IV, key)
+		if err != nil {
+			t.Error(err)
+		}
+		decryptedData, err := nse.Decrypt(ciphertext, salt, IV, key)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if decryptedData == nil {
+			t.Errorf("Decrypted data is nil")
+		}
+		if !bytes.Equal(data, decryptedData) {
+			t.Errorf("%v is not %v", data, decryptedData)
+		}
+	}
+}
